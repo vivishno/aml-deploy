917a0feea543d024ba39337e0ec656af6c0bcaf9
vivishno <64971172+vivishno@users.noreply.github.com>
2020-08-06 12:55:41 +0530

    Update main.py

--- code/main.py
+++ code/main.py
@@ -228,8 +228,8 @@ def main():
             deployment_target = None
         if deployment_target == None:
            try:
-                attach_config = AksCompute.attach_configuration(resource_group ="testaks",cluster_name = "myakas2",cluster_purpose = AksCompute.ClusterPurpose.DEV_TEST)
-                deployment_target= ComputeTarget.attach(ws, 'myakas2', attach_config)
+                attach_config = AksCompute.attach_configuration(resource_group ="testaks",cluster_name = "myakas",cluster_purpose = AksCompute.ClusterPurpose.DEV_TEST)
+                deployment_target= ComputeTarget.attach(ws, 'myakas', attach_config)
                 deployment_target.wait_for_completion(show_output = True)
                 print("here------------------------------------2")
            except ComputeTargetException:
